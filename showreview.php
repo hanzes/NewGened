1032afcf2b136c4f478bd77d7b326d143c1e3499
hanzes <za_cena@hotmail.com>
2016-04-20 21:44:28 +0700

    update หน้า show นิดหน่อย

--- showreview.php
+++ showreview.php
@@ -153,28 +153,18 @@ $rname = array('reviewer'=> $doc['reviewer'],'grade' => $doc['grade'],'rate' => 
 			<div class="row">
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
-						<h3><i class="fa fa-thumbs-o-up"></i> <span class="st-counter">365</span></h3>
-						<p>Happy Clients</p>
+						<h3><i class="fa fa-thumbs-o-up"></i> <span class="st-counter"><?php sprintf("%.2f", ($rate/$num)); ?></span></h3>
+						<p>Average Rating</p>
 					</div>
 				</div>
 				<div class="col-sm-6 col-md-3">
 					<div class="fun-fact text-center">
-						<h3><i class="fa fa-briefcase fa-6"></i> <span class="st-counter">73987</span></h3>
-						<p>Completed Projects</p>
+						<h3><i class="fa fa-briefcase fa-6"></i> <span class="st-counter"><?php echo $num; ?></span></h3>
+						<p>Reviewer Number</p>
 					</div>
 				</div>
-				<div class="col-sm-6 col-md-3">
-					<div class="fun-fact text-center">
-						<h3><i class="fa fa-coffee"></i> <span class="st-counter">297345</span></h3>
-						<p>Cups of Coffee</p>
-					</div>
-				</div>
-				<div class="col-sm-6 col-md-3">
-					<div class="fun-fact text-center">
-						<h3><i class="fa fa-code"></i> <span class="st-counter">9823686</span></h3>
-						<p>Lines of Code</p>
-					</div>
-				</div>
+		
+			
 			</div>
 		</div>
 	</section>
