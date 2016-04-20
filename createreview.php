728b5de8a310d3fb58852a54aed3b330609483dc
hanzes <za_cena@hotmail.com>
2016-04-20 23:49:22 +0700

    create review จ้า

--- createreview.php
+++ createreview.php
@@ -29,34 +29,13 @@
 	<![endif]-->
 	
 </head>
-<body>
-<header id="header">
-		<nav class="navbar st-navbar navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#st-navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-				    	<span class="icon-bar"></span>
-				    	<span class="icon-bar"></span>
-				    	<span class="icon-bar"></span>
-					</button>
-					<a class="logo" href="index.html"><img src="images/logo.png" alt=""></a>
-				</div>
 
-				<div class="collapse navbar-collapse" id="st-navbar-collapse">
-					<ul class="nav navbar-nav navbar-right">
-				    	<li><a href="#header">Home</a></li>
-				    	<li><a href="#services">Services</a></li>
-				    	<li><a href="#our-works">Works</a></li>
-				    	<li><a href="#pricing">Pricing</a></li>
-				    	<li><a href="#our-team">Team</a></li>
-				    	<li><a href="#contact">Contact</a></li>
-				    	<li><a href="blog.html">Blog</a></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container -->
-		</nav>
-	</header>
+<body>
+<?php 
+
+					include 'nav.php'; 	
+		  
+?>
 	<br>
 <section id="services">
 		<div class="container">
